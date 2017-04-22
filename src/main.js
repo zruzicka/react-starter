f68559daeca7d70170a38356c548b294de9da769
Zbynek Ruzicka <ZRuzicka@email.cz>
2017-04-22 23:01:07 +0200

    Starter: integrated React with Counter component.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 console.log('Starter says Hello!');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './Counter';
+ 
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(
+    React.createElement(Counter),
+    document.getElementById('root')
+  );
+});
